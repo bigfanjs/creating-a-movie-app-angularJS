c469cb121234cf097627b83c0293c22060f48f7c
AdelJS <bigfanjs@gmail.com>
2016-11-14 11:10:36 +0000

    creating a placehlder server.js

--- server.js
+++ server.js
@@ -5,7 +5,38 @@ const
   express = require('express'),
   app = express();
 
+const tempData = [
+  { title: "Titanic",
+    type: 'Romantic',
+    runningTime: '123mins',
+    releaseDate: '23/04/1999',
+    cover: {
+      path: 'avatar.jpg',
+      name: 'avatar' },
+    story: 'some text for the movie\'s story' },
+  { title: "Death pool",
+    type: 'Action',
+    runningTime: '103mins',
+    releaseDate: '23/04/2014',
+    cover: {
+      path: 'interstellar.jpg',
+      name: 'interstellar' },
+    story: 'some text for the movie\'s story' },
+  { title: "Showlin Soccer",
+    type: 'comidian',
+    runningTime: '132mins',
+    releaseDate: '23/04/2006',
+    cover: {
+      path: 'the-campaign.jpg',
+      name: 'the-campaign' },
+    story: 'some text for the movie\'s story' }
+];
+
 app.use(express.static(path.join(__dirname, './public')));
+
+app.get('/api/movies', function ( req, res ) {
+  res.json( tempData );
+});
 
 app.listen(8080, function () {
   console.log('Listening on port 8080');
