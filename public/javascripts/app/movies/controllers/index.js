6442634045d04fe5d08e4681ad60edd9ef4f2af8
AdelJS <bigfanjs@gmail.com>
2016-11-23 17:01:56 +0000

    creating the controllers index file

---
+++ public/javascripts/app/movies/controllers/index.js
@@ -0,0 +1,5 @@
+const movieApp = require('angular').module('movieApp');
+
+movieApp.controller('moviesListCtrl', require('./movies-list-ctrl'));
+// movieApp.controller('movieListItemCtrl', require('./movie-list-item-ctrl'));
+// movieApp.controller('filterBarCtrl', require('./filter-bar-ctrl'));
