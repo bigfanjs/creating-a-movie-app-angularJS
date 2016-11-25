b4552f17c21538c041d6000b1278909c56a6545d
AdelJS <bigfanjs@gmail.com>
2016-11-25 09:11:30 +0000

    provide the page count filter to the top level moviesApp module by passing it to the filters/index

--- public/javascripts/app/movies/filters/index.js
+++ public/javascripts/app/movies/filters/index.js
@@ -2,3 +2,4 @@ const movieApp = require('angular').module('movieApp');
 
 movieApp.filter('uniq', require('./uniq'));
 movieApp.filter('range', require('./range'));
+movieApp.filter('pageCount', require('./page-count'));
