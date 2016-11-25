f366cc1691c97259c24538692c16a0a536f68109
AdelJS <bigfanjs@gmail.com>
2016-11-25 15:42:34 +0000

    updating the movies list controller [getting the class name depending on the current page]

--- public/javascripts/app/movies/controllers/movies-list-ctrl.js
+++ public/javascripts/app/movies/controllers/movies-list-ctrl.js
@@ -1,6 +1,8 @@
 module.exports = function ( $scope, $http ) {
   $scope.data = [];
 
+  $scope.pageSize = 12;
+  $scope.selectedPage = 1;
   $scope.title = null;
 
   $http.get('/api/movies/').then(response => {
@@ -27,5 +29,13 @@ module.exports = function ( $scope, $http ) {
     const rege = new RegExp(type, 'i');
 
     return type === null || movie.type.match( rege );
+  };
+
+  $scope.selectPage = function ( page ) {
+    $scope.selectedPage = page;
+  };
+
+  $scope.getPageClass = function ( page ) {
+    return $scope.selectedPage == page ? 'btn-primary' : '';
   };
 };
