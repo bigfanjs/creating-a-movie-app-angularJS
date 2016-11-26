6008c3901b421318aec2330ba8c65faf724c4124
AdelJS <bigfanjs@gmail.com>
2016-11-26 10:40:35 +0000

    creating movies route

---
+++ routes/movies.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const Movie = require('../models').Movie;
+
+exports.form = function (req, res, next) {
+  res.render('movie', { title: 'Movie', msg: req.flash('error') });
+};
+
+exports.listMovies = function (req, res, next) {
+  Movie.find({}, (err, movies) => {
+    if ( err ) return next( err );
+
+    res.json( movies );
+  });
+};
+
+exports.viewMovie = function (req, res, next) {
+  Movie.findOne({ _id: req.params.id }, (err, movie) => {
+    if ( err ) return next( err );
+
+    res.json( movie );
+  });
+};
+
+exports.createMovie = function (req, res, next) {
+  Movie.create(req.body, (err, movie) => {
+    if ( err ) return next();
+
+    res.json( movie );
+  });
+};
+
+exports.updateMovies = function (req, res, next) {
+  Movie.update(
+    { _id: req.param.id },
+    req.body,
+    { multi: false },
+    (err, movie) => {
+    if ( err ) return next( err );
+
+    res.json( movie );
+  });
+};
+
+exports.deleteMovie = function (req, res, next) {
+  Movie.remove({ _id: req.param.id }, (err, movie) => {
+    if ( err ) return next( err );
+
+    res.json( movie );
+  });
+};
