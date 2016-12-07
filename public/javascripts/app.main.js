5cf8a3b467de526c5080f0e7265e74bff7acc1bf
AdelJS <bigfanjs@gmail.com>
2016-12-07 17:36:04 +0000

    enabling teh HTML5 History API by passing true to the html5Mode

--- public/javascripts/app.main.js
+++ public/javascripts/app.main.js
@@ -4,6 +4,10 @@ require('angular-route');
 
 const app = angular.module('movieApp', ['ngRoute']);
 
+app.config($locationProvider => {
+  $locationProvider.html5Mode(true);
+});
+
 require('./app/movies/router');
 require('./app/movies/controllers');
 require('./app/movies/filters');
