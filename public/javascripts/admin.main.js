65f391c9da121bcb7ed24ef091a94d6561947c98
AdelJS <bigfanjs@gmail.com>
2016-12-11 16:47:31 +0000

    enabling the html5 mode in the admin page

--- public/javascripts/admin.main.js
+++ public/javascripts/admin.main.js
@@ -4,5 +4,12 @@ require('angular-route');
 
 const app = angular.module('admin', ['ngRoute']);
 
+app.config($locationProvider => {
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+  });
+});
+
 require('./app/admin-login/router');
 require('./app/admin-login/controllers');
