4f3bfaadfa0a843aeda6e734cd034136e4c9d4df
AdelJS <bigfanjs@gmail.com>
2016-12-14 12:45:29 +0000

    creating a new admin login controller

--- public/javascripts/app/movies/controllers/index.js
+++ public/javascripts/app/movies/controllers/index.js
@@ -3,5 +3,4 @@ const movieApp = require('angular').module('movieApp');
 movieApp.constant('MovieListPageCount', 12);
 
 movieApp.controller('moviesListCtrl', require('./movies-list-ctrl'));
-// movieApp.controller('movieListItemCtrl', require('./movie-list-item-ctrl'));
-// movieApp.controller('filterBarCtrl', require('./filter-bar-ctrl'));
+movieApp.controller('loginCtrl', require('./login-ctrl'));
