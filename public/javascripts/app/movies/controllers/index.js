9ef335494573da8b651d151cd1e5b1e0b21c4938
Adel Jojo <bigfanjs@gmail.com>
2016-12-22 04:24:00 -0800

    removing the login controller from the movies app

--- public/javascripts/app/movies/controllers/index.js
+++ public/javascripts/app/movies/controllers/index.js
@@ -3,4 +3,3 @@ const movieApp = require('angular').module('movieApp');
 movieApp.constant('MovieListPageCount', 12);
 
 movieApp.controller('moviesListCtrl', require('./movies-list-ctrl'));
-movieApp.controller('loginCtrl', require('./login-ctrl'));
