4ebaaf3cd9c5fb484db540b7f2327dc887ece795
Adel Jojo <bigfanjs@gmail.com>
2016-12-22 04:07:43 -0800

    moving the the auth service from the movies app to the admin-login app and updating the authentication service

---
+++ public/javascripts/app/admin-login/services/auth-service.js
@@ -0,0 +1,46 @@
+module.exports = function ($http, $q) {
+  return {
+    isAuth: false,
+    login: function (username, password) {
+      const dfd = $q.defer();
+
+      $http
+        .post('/admin/login', {username, password})
+        .then(
+          (data, status) => {
+            if (status === 200) {
+              this.isAuth = true;
+              dfd.resolve();
+            } else {
+              this.isAuth = false;
+              dfd.reject();
+            }
+          },
+          function ( err ) {
+            dfd.reject( err );
+          }
+        );
+
+      return dfd.promise;
+    },
+    logout: function () {
+      const dfd = $.defer();
+
+      $http
+        .get('/admin/logout')
+        .then(
+          (data, status) => {
+            if (status == 200) {
+              dfd.resolve();
+              this.isAuth = false;
+            } 
+          },
+          function (err, status) {
+            dfd.reject( err );
+          }
+        );
+
+      return dfd.promise;
+    }
+  };
+};
