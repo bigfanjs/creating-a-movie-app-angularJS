e5eb4b776db71165c62160f091ca05af66fe81db
Adel Jojo <bigfanjs@gmail.com>
2016-12-24 09:30:35 +0000

    creating a monolithic controller [main-ctrl]

---
+++ public/javascripts/app/movies/controllers/main-ctrl.js
@@ -0,0 +1,35 @@
+module.exports = function ($scope) {
+  $scope.pageSize = MovieListPageCount;
+  $scope.selectedPage = 1;
+  $scope.title = null;
+  
+  $scope.lookUp = function ( title ) {
+    $scope.title = title;
+  };
+
+  $scope.titleFilter = function ( movie ) {
+    const rege = new RegExp($scope.title, 'i');
+
+    return $scope.title === null || movie.title.match( rege );
+  };
+
+  $scope.typeFilter = function ( movie ) {
+    var type = null;
+
+    if ( $scope.movie ) {
+      type = $scope.movie.type;
+    }
+
+    const rege = new RegExp(type, 'i');
+
+    return type === null || movie.type.match( rege );
+  };
+
+  $scope.selectPage = function ( page ) {
+    $scope.selectedPage = page;
+  };
+
+  $scope.getPageClass = function ( page ) {
+    return $scope.selectedPage == page ? 'btn-primary' : '';
+  };
+};
