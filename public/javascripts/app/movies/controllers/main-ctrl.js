5815c5a79254c872013eb7b2187778aafc9c5520
Adel Jojo <bigfanjs@gmail.com>
2016-12-24 12:36:03 +0000

    removing the typeFilter method handler from and adding the .get() to the monolothic controller

--- public/javascripts/app/movies/controllers/main-ctrl.js
+++ public/javascripts/app/movies/controllers/main-ctrl.js
@@ -1,8 +1,12 @@
 module.exports = function ($scope) {
-  $scope.pageSize = MovieListPageCount;
+  $scope.data = [];
   $scope.selectedPage = 1;
   $scope.title = null;
-  
+
+  $http.get('/api/movies/').then(response => {
+    $scope.data.movies = response.data;
+  });
+
   $scope.lookUp = function ( title ) {
     $scope.title = title;
   };
@@ -11,18 +15,6 @@ module.exports = function ($scope) {
     const rege = new RegExp($scope.title, 'i');
 
     return $scope.title === null || movie.title.match( rege );
-  };
-
-  $scope.typeFilter = function ( movie ) {
-    var type = null;
-
-    if ( $scope.movie ) {
-      type = $scope.movie.type;
-    }
-
-    const rege = new RegExp(type, 'i');
-
-    return type === null || movie.type.match( rege );
   };
 
   $scope.selectPage = function ( page ) {
