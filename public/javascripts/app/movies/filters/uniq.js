ec3dfce7e7a3f928634ae2ea4266623186ab1b91
Adel Jojo <bigfanjs@gmail.com>
2017-01-16 09:30:20 +0000

    updating the movies app uniq filter

--- public/javascripts/app/movies/filters/uniq.js
+++ public/javascripts/app/movies/filters/uniq.js
@@ -2,11 +2,8 @@ module.exports = function () {
   return (data, prop) => {
     if ( Array.isArray( data ) && typeof prop === 'string') {
       return data.slice(0).filter((movie, idx, movies) => {
-        var arr = movies.map(obj => {
-          return obj[ prop ];
-        });
-
-        return arr.indexOf( movie[ prop ] ) == idx;
+        const props = movies.map(obj => obj[ prop ]);
+        return props.indexOf( movie[ prop ] ) == idx;
       });
     } else {
       return data;
