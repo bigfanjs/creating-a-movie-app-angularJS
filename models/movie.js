3ad968eaea40f9e89a2704e65746d2fd189ac4e0
Adel Jojo <bigfanjs@gmail.com>
2017-01-16 14:50:42 +0000

    updating the server-side movie model

--- models/movie.js
+++ models/movie.js
@@ -3,19 +3,38 @@
 module.exports = function (mongoose, db) {
   const schema = new mongoose.Schema({
     title: String,
-    type: String,
+    genre: String,
     runningTime: String,
-    releaseYear: Number,
-    cover: {
-      path: String,
-      name: String
+    releaseDate: {
+      month: String,
+      day: String,
+      year: String
     },
-    story: String,
+    overview: String,
+    director: String,
+    boxOffice: String,
+    budget: String,
+    country: String,
+    language: String,
+    cast: [{
+      actor: String,
+      character: String,
+      avatar: {
+        file: String,
+        url: String
+      },
+      star: Boolean,
+      gender: String
+    }],
+    cover: {
+      file: String,
+      url: String
+    },
     meta: {
       dateAdded: String,
       favorites: Number,
       watches: Number
-    }
+    } 
   });
 
   return db.model('movie', schema);
