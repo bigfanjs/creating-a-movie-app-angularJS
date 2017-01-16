2c0386f6d22319ba5948548d895d51a6aba06e09
Adel Jojo <bigfanjs@gmail.com>
2017-01-16 14:59:21 +0000

    supporting the nested objects in custom the uniq filter

--- public/javascripts/app/movies/filters/uniq.js
+++ public/javascripts/app/movies/filters/uniq.js
@@ -1,9 +1,22 @@
+const isObject = require('lodash/isObject');
+
 module.exports = function () {
-  return (data, prop) => {
-    if ( Array.isArray( data ) && typeof prop === 'string') {
+  return function(data, field, deep_field) {
+    var embedded = null;
+
+    if (arguments.length === 3) {
+      embedded = field;
+      field = deep_field;
+      deep_field = null;
+    }
+
+    if (Array.isArray(data) && typeof field === 'string') {
       return data.slice(0).filter((movie, idx, movies) => {
-        const props = movies.map(obj => obj[ prop ]);
-        return props.indexOf( movie[ prop ] ) == idx;
+        const props = movies.map(obj => {
+          return embedded ? obj[embedded][field] : obj[field];
+        });
+        const prop = embedded ? movie[embedded][field] : movie[field];
+        return props.indexOf( prop ) == idx;
       });
     } else {
       return data;
