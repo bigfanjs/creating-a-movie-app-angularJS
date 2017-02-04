f14e1954829c121236d9962dfc9f24e77abc4db8
Adel Jojo <bigfanjs@gmail.com>
2017-02-04 16:20:18 +0000

    updating the controllers' prvider

--- public/javascripts/app/movies/controllers/index.js
+++ public/javascripts/app/movies/controllers/index.js
@@ -2,7 +2,6 @@ const movieApp = require('angular').module('movieApp');
 
 movieApp.constant('movieListPageCount', 12);
 movieApp.constant('adminPageCount', 30);
-movieApp.constant('baseURL', '/api/movies/');
 
 movieApp.controller('mainCtrl', require('./main-ctrl'));
 movieApp.controller('moviesListCtrl', require('./movies-list-ctrl'));
