def79337deb6b8536135ccb7f402862949faa9fc
Adel Jojo <bigfanjs@gmail.com>
2017-02-04 16:23:08 +0000

    updating the movies app top lever controller

--- public/javascripts/app/movies/controllers/main-ctrl.js
+++ public/javascripts/app/movies/controllers/main-ctrl.js
@@ -1,17 +1,13 @@
-module.exports = function ($scope, Movie) {
+module.exports = function ($scope) {
   $scope.selectedPage = 1;
   $scope.movie = { title: null };
-
-  $scope.lookUp = function ( title ) {
-    $scope.title = title;
-  };
 
   $scope.titleFilter = function ( movie ) {
     const
       title = $scope.movie.title,
       rege = new RegExp(title, 'i');
 
-    return title === null || movie.title.match( rege );
+    return title === null || (movie.title || '').match( rege );
   };
 
   $scope.selectPage = function ( page ) {
